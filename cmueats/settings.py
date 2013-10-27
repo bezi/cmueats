0bf2a8178b038306d512dcfb26cbc9ec6e84eea0
Justin Gallagher <justingallag@gmail.com>
2013-10-26 21:45:53 -0400

    Added debug instructions to README.md

--- cmueats/settings.py
+++ cmueats/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['www.cmueats.com', 'cmueats.herokuapp.com', 'localhost',]
+ALLOWED_HOSTS = ['*', 'www.cmueats.com', 'cmueats.herokuapp.com', 'localhost',]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
