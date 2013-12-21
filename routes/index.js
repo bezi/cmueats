7f3d3ccf06b654b729a6aae93fb2688644bf625f
Justin Gallagher <justingallag@gmail.com>
2013-12-21 15:42:37 -0500

    Updated gitignore, created helloworld page

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.helloworld = function(req, res){
+  res.render('helloworld', { title: 'Hello, World!' })
+};
