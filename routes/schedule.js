d87e08fc79c64980c87b623869d615502bbc8aa8
Oscar Bezi <oscar.bezi@gmail.com>
2013-12-21 20:32:50 -0500

    Made the app run again.  Better test stuff.

--- routes/schedule.js
+++ routes/schedule.js
@@ -5,11 +5,37 @@ module.exports = function(req, res){
     res.render('index', {
         openLoc: [
             {
-                name: "Gallo",
-                location:
+                name: "Gallo1",
+                location: "here",
+                closeDate: Number(moment().format("X"))
+            },
+            {
+                name: "Gallo2",
+                location: "here",
+                closeDate: Number(moment().format("X"))
+            },
+            {
+                name: "Gallo3",
+                location: "here",
+                closeDate: Number(moment().format("X"))
+            },
+            {
+                name: "Gallo4",
+                location: "here",
+                closeDate: Number(moment().format("X"))
             }
         ],
         closedLoc: [
+            {
+                name: "Gallo5",
+                location: "here",
+                closeDate: Number(moment().format("X"))
+            },
+            {
+                name: "Gallo6",
+                location: "here",
+                closeDate: Number(moment().format("X"))
+            }
             
         ]
     });
