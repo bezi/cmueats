4d8c3d64cfc948aafd963870c4efc54d16eea0f7
Eric Cheng <ericcheng@hey.com>
2021-11-20 20:32:58 -0500

    Implemented menus

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 "use strict";
 
 function getEateryTagline(e, t, r) {
-  var n = moment(r).tz("America/New_York"),
+  var n = moment(r)
+    .tz("America/New_York"),
     a = t ? "Closes " : "Opens ";
   return (
-    (a += n.fromNow().charAt(0).toLowerCase() + n.fromNow().slice(1)),
-    (a += " (" + n.calendar().toLowerCase() + ")")
+    (a += n.fromNow()
+      .charAt(0)
+      .toLowerCase() + n.fromNow()
+      .slice(1)),
+    (a += " (" + n.calendar()
+      .toLowerCase() + ")")
   );
 }
 
-var getAPIFmtConversion = function (e) {
+var getAPIFmtConversion = function(e) {
     var t = new Date(e);
-    return function (e) {
+    return function(e) {
       var r = new Date(t),
         n = (e.day + 7 - r.getDay()) % 7;
       return (
@@ -24,7 +29,7 @@ var getAPIFmtConversion = function (e) {
       );
     };
   },
-  dateToAPIFmt = function (e) {
+  dateToAPIFmt = function(e) {
     var t = new Date(e);
     return {
       day: t.getDay(),
@@ -32,34 +37,34 @@ var getAPIFmtConversion = function (e) {
       minute: t.getMinutes(),
     };
   },
-  apiFmtToInt = function (e) {
+  apiFmtToInt = function(e) {
     return 1e4 * e.day + 100 * e.hour + e.minute;
   },
-  genEateryMetadata = function (e, t) {
+  genEateryMetadata = function(e, t) {
     var r = e.times,
       n = apiFmtToInt(dateToAPIFmt(t)),
       a = r
-        .map(function (e) {
-          var t = apiFmtToInt(e.start),
-            r = apiFmtToInt(e.end);
-          return r > t ? n >= t && r > n : n >= t || r > n;
-        })
-        .reduce(function (e, t) {
-          return e || t;
-        }, !1),
+      .map(function(e) {
+        var t = apiFmtToInt(e.start),
+          r = apiFmtToInt(e.end);
+        return r > t ? n >= t && r > n : n >= t || r > n;
+      })
+      .reduce(function(e, t) {
+        return e || t;
+      }, !1),
       i = getAPIFmtConversion(t),
       s = r
-        .map(function (e) {
-          return i(a ? e.end : e.start);
-        })
-        .sort()[0];
+      .map(function(e) {
+        return i(a ? e.end : e.start);
+      })
+      .sort()[0];
     return {
       isOpen: a,
       nextTime: s,
     };
   },
   Eatery = React.createClass({
-    render: function () {
+    render: function() {
       var e,
         f,
         t = getEateryTagline(
@@ -67,65 +72,67 @@ var getAPIFmtConversion = function (e) {
           this.props.eatery.isOpen,
           this.props.eatery.nextTime
         );
+      if (this.props.eatery.menu !== undefined) {
+        var m = React.createElement("a", {
+            className: "menu-text",
+            href: this.props.eatery.menu,
+          }, this.props.eatery.name.toLowerCase())
+      } else {
+        m = this.props.eatery.name.toLowerCase();
+      };
+      console.log(m);
       return (
-        (e = this.props.eatery.isOpen ? "open_eatery" : "closed_eatery"),
+        (e = this.props.eatery.isOpen ? "open-eatery" : "closed-eatery"),
         (f = this.props.eatery.isOpen ? "open" : "closed"),
         React.createElement(
-          "div",
-          {
+          "div", {
             className: "card",
           },
           React.createElement(
-            "div",
-            {
+            "div", {
               className: "card-header",
             },
             React.createElement("span", {
               className: e,
             }),
             React.createElement(
-              "span",
-              {
+              "span", {
                 className: f,
               },
               t
             )
           ),
           React.createElement(
-            "div",
-            {
+            "div", {
               className: "card-body",
             },
             React.createElement(
-              "h5",
-              {
+              "h4", {
                 className: "card-title",
               },
-              this.props.eatery.name.toLowerCase()
+              m
             ),
             React.createElement(
-              "p",
-              {
+              "p", {
                 className: "card-subtitle pb-2 tagline text-muted",
               },
               this.props.eatery.location
             ),
             React.createElement(
-              "p",
-              {
+              "p", {
                 className: "card-text",
               },
               this.props.eatery.short_description
-            )
+            ),
           )
         )
       );
     },
   }),
   EateryList = React.createClass({
-    render: function () {
+    render: function() {
       var e = this,
-        t = this.props.eateryList.map(function (t) {
+        t = this.props.eateryList.map(function(t) {
           if (t.name.length > 0) {
             return React.createElement(Eatery, {
               key: t.name,
@@ -135,8 +142,7 @@ var getAPIFmtConversion = function (e) {
           }
         });
       return React.createElement(
-        "div",
-        {
+        "div", {
           className: "eateryList card-columns",
         },
         t
@@ -144,57 +150,59 @@ var getAPIFmtConversion = function (e) {
     },
   }),
   EateryContainer = React.createClass({
-    getInitialState: function () {
+    getInitialState: function() {
       return {
         eateries: [],
-        currentTime: new Date(Date.now()).toJSON(),
+        currentTime: new Date(Date.now())
+          .toJSON(),
       };
     },
-    reloadFromServer: function () {
+    reloadFromServer: function() {
       var e = this;
-      $.getJSON(this.props.url, function (t) {
+      $.getJSON(this.props.url, function(t) {
         return e.setState({
           eateries: t.locations,
-          currentTime: new Date(Date.now()).toJSON(),
+          currentTime: new Date(Date.now())
+            .toJSON(),
         });
       });
     },
-    reloadUI: function () {
+    reloadUI: function() {
       this.setState({
         eateries: this.state.eateries,
-        currentTime: new Date(Date.now()).toJSON(),
+        currentTime: new Date(Date.now())
+          .toJSON(),
       });
     },
-    componentDidMount: function () {
+    componentDidMount: function() {
       var e = 5e3,
         t = 25e4;
       this.reloadFromServer(),
         setInterval(this.reloadUI, e),
         setInterval(this.reloadFromServer, t);
     },
-    render: function () {
+    render: function() {
       var e = this,
-        t = this.state.eateries.map(function (t) {
+        t = this.state.eateries.map(function(t) {
           var r = genEateryMetadata(t, e.state.currentTime);
           return (t.isOpen = r.isOpen), (t.nextTime = r.nextTime), t;
         }),
         r = t
-          .filter(function (e) {
-            return e.isOpen;
-          })
-          .sort(function (e, t) {
-            return new Date(e.nextTime) - new Date(t.nextTime);
-          }),
+        .filter(function(e) {
+          return e.isOpen;
+        })
+        .sort(function(e, t) {
+          return new Date(e.nextTime) - new Date(t.nextTime);
+        }),
         n = t
-          .filter(function (e) {
-            return !e.isOpen;
-          })
-          .sort(function (e, t) {
-            return new Date(e.nextTime) - new Date(t.nextTime);
-          });
+        .filter(function(e) {
+          return !e.isOpen;
+        })
+        .sort(function(e, t) {
+          return new Date(e.nextTime) - new Date(t.nextTime);
+        });
       return React.createElement(
-        "div",
-        {
+        "div", {
           className: "eateryContainer",
         },
         React.createElement(EateryList, {
