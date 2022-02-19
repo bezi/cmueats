91486eb4be85431a03bb232d41bc6d3ab6b7ecc3
Gram Liu <gram.liu226@gmail.com>
2022-02-19 18:21:48 -0500

    Computed time to open/close

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import queryLocations from './util/queryLocations';
 
 function App() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    queryLocations().then((parsedLocations) => {
+      setLocations(parsedLocations);
+    })
+  }, []);
+
   return (
     <div className="App">
-      
     </div>
   );
 }
