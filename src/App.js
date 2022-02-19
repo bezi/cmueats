d79fbda116999ab96e6b8de8461cfcb1d7433141
Gram Liu <gram.liu226@gmail.com>
2022-02-19 15:32:22 -0500

    Added favicons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const BASE_URL = "https://apis.dining.scottylabs.org/locations";
+
+async function queryLocations() {
+  const { data: locations } = axios.get(BASE_URL);
+  return locations;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
